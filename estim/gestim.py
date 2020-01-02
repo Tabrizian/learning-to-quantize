194a168b3877ddbb128e2acec43b26d18b06f181
Iman Tabrizian <tabrizian@outlook.com>
2020-01-02 01:30:19 -0500

    Add the estimation method

--- estim/gestim.py
+++ estim/gestim.py
@@ -3,7 +3,10 @@ import torch.nn
 import torch.multiprocessing
 import numpy as np
 import math
-from cuquant import qdq_gpu
+
+import copy
+import logging
+
 from data import InfiniteLoader
 
 
@@ -13,23 +16,71 @@ class GradientEstimator(object):
         self.model = None
         self.data_loader = data_loader
         self.tb_logger = tb_logger
+        self.niters = 0
         self.random_indices = None
+
+    def update_niters(self, niters):
+        self.niters = niters
 
     def init_data_iter(self):
         self.data_iter = iter(InfiniteLoader(self.data_loader))
         self.estim_iter = iter(InfiniteLoader(self.data_loader))
 
-    def grad(self, model_new, in_place=False):
+    def snap_batch(self, model):
+        pass
+
+    def update_sampler(self):
+        pass
+
+    def snap_online(self, model):
+        pass
+
+    def grad(self, model_new, in_place=False, data=None):
         raise NotImplementedError('grad not implemented')
 
     def grad_estim(self, model):
-        # insuring continuity of data seen in training
+        # ensuring continuity of data seen in training
         # TODO: make sure sub-classes never use any other data_iter, e.g. raw
         dt = self.data_iter
         self.data_iter = self.estim_iter
         ret = self.grad(model)
         self.data_iter = dt
-        return ret        
+        return ret
+
+    def get_Ege_var(self, model, gviter):
+        # estimate grad mean and variance
+        Ege = [torch.zeros_like(g) for g in model.parameters()]
+        for i in range(gviter):
+            ge = self.grad_estim(model)
+            for e, g in zip(Ege, ge):
+                e += g
+
+        for e in Ege:
+            e /= gviter
+
+        n = 0
+        for e in Ege:
+            n += e.numel()
+        nw = sum([w.numel() for w in model.parameters()])
+        var_e = 0
+        Es = [torch.zeros_like(g) for g in model.parameters()]
+        En = [torch.zeros_like(g) for g in model.parameters()]
+        for i in range(gviter):
+            ge = self.grad_estim(model)
+            # TODO: is variance important?
+            v = sum([(gg-ee).pow(2).sum() for ee, gg in zip(Ege, ge)])
+            for s, e, g, n in zip(Es, Ege, ge, En):
+                s += g.pow(2)
+                n += (e-g).pow(2)
+            var_e += v/nw
+
+        var_e /= gviter
+        # Division by gviter cancels out in ss/nn
+        snr_e = sum(
+                [((ss+1e-10).log()-(nn+1e-10).log()).sum()
+                    for ss, nn in zip(Es, En)])/nw
+        nv_e = sum([(nn/(ss+1e-7)).sum() for ss, nn in zip(Es, En)])/nw
+        return Ege, var_e, snr_e, nv_e
 
     def flatten(self, gradient):
         flattened_parameters = []
@@ -179,44 +230,17 @@ class GradientEstimator(object):
         total_variance_unconcatenated = sum([torch.sum(variance) / variance.numel() for variance in variance_estimates_unconcatenated]) / len(mean_estimates)
 
         return variances, means, total_mean, total_variance, total_variance_normalized, total_mean_normalized, total_mean_unconcatenated, total_variance_unconcatenated
-        
-    def get_Ege_var(self, model, gviter):
-        Ege = [torch.zeros_like(g) for g in model.parameters()]
-        for i in range(gviter):
-            ge = self.grad_estim(model)
-            for e, g in zip(Ege, ge):
-                e += g
-
-        for e in Ege:
-            e /= gviter
-
-        z = 0
-        n = 0
-        for e in Ege:
-            z += (e.abs() < self.opt.adam_eps).sum().item()
-            n += e.numel()
-        nw = sum([w.numel() for w in model.parameters()])
-        var_e = 0
-        Es = [torch.zeros_like(g) for g in model.parameters()]
-        En = [torch.zeros_like(g) for g in model.parameters()]
-        for i in range(gviter):
-            ge = self.grad_estim(model)
-            v = sum([(gg-ee).pow(2).sum() for ee, gg in zip(Ege, ge)])
-            for s, e, g, n in zip(Es, Ege, ge, En):
-                s += g.pow(2)
-                n += (e-g).pow(2)
-            var_e += v/nw
-
-        var_e /= gviter
-        # Division by gviter cancels out in ss/nn
-        snr_e = sum(
-                [((ss+1e-10).log()-(nn+1e-10).log()).sum()
-                    for ss, nn in zip(Es, En)])/nw
-        nv_e = sum([(nn/(ss+1e-7)).sum() for ss, nn in zip(Es, En)])/nw
-        return Ege, var_e, snr_e, nv_e
-
     def state_dict(self):
         return {}
 
     def load_state_dict(self, state):
         pass
+
+    def snap_model(self, model):
+        logging.info('Snap Model')
+        if self.model is None:
+            self.model = copy.deepcopy(model)
+            return
+        # update sum
+        for m, s in zip(model.parameters(), self.model.parameters()):
+            s.data.copy_(m.data)
