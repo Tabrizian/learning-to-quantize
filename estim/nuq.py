d5880c892c5541d1c9a3a9f170da6c3b41feba1e
Iman Tabrizian <tabrizian@outlook.com>
2020-01-23 13:40:30 -0500

    Use numpy quantization

--- estim/nuq.py
+++ estim/nuq.py
@@ -4,14 +4,14 @@ import torch.multiprocessing
 
 from args import opt_to_nuq_kwargs
 from .gestim import GradientEstimator
-from nuq.quantize import QuantizeMultiBucket
+from nuq.quantize import QuantizeNumPy
 
 
 class NUQEstimator(GradientEstimator):
     def __init__(self, *args, **kwargs):
         super(NUQEstimator, self).__init__(*args, **kwargs)
         self.init_data_iter()
-        self.qdq = QuantizeMultiBucket(**opt_to_nuq_kwargs(self.opt))
+        self.qdq = QuantizeNumPy(**opt_to_nuq_kwargs(self.opt))
         self.ngpu = self.opt.nuq_ngpu
         self.acc_grad = None
 
@@ -28,10 +28,11 @@ class NUQEstimator(GradientEstimator):
                 a.zero_()
 
         for i in range(self.ngpu):
+            print('Salammm', i)
             model.zero_grad()
             data = next(self.data_iter)
-
             loss = model.criterion(model, data)
+            print('Shape of loss', loss.shape)
             grad = torch.autograd.grad(loss, model.parameters())
 
             with torch.no_grad():
