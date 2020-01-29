Fix the variance issue

estim/nuq.py
@@ -42,8 +42,8 @@ class NUQEstimator(GradientEstimator):
 
             with torch.no_grad():
                 for g, a in zip(grad, mean):
-                    if len(a.shape) != 1:
-                        a += g
+                    # if len(a.shape) != 1:
+                    a += g
         
         nw = sum([w.numel() for w in model.parameters()])
         for i, a in enumerate(mean):
@@ -69,8 +69,8 @@ class NUQEstimator(GradientEstimator):
 
             with torch.no_grad():
                 for ee, gg in zip(mean, grad):
-                    if len(ee.shape) != 1:
-                        variance += (gg-ee).pow(2).sum()
+                    # if len(ee.shape) != 1:
+                    variance += (gg-ee).pow(2).sum()
                 total_variance += variance / nw
         total_variance /= iterations
         total_mean = sum([item.sum() for item in mean])