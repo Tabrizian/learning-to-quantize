9d8e1a7b5300921b1e8aed93738d7f3bdf339cc4
Iman Tabrizian <tabrizian@outlook.com>
2020-06-02 18:01:02 -0400

    add the new plots

--- log_plotter.py
+++ log_plotter.py
@@ -144,7 +144,7 @@ class OOMFormater(mtick.ScalarFormatter):
         # first estimate:
         sigfigs = max(0, 4 - loc_range_oom)
         # refined estimate:
-        thresh = 1e-6 * 10 ** (loc_range_oom)
+        thresh = 1e-3 * 10 ** (loc_range_oom)
         while sigfigs >= 0:
             if np.abs(locs - np.round(locs, decimals=sigfigs)).max() < thresh:
                 sigfigs -= 1
@@ -156,7 +156,7 @@ class OOMFormater(mtick.ScalarFormatter):
             self.format = r'$\mathdefault{%s}$' % self.format
 
 def plot_tag(data, plot_f, run_names, tag_name, lg_tags, ylim=None, color0=0,
-             ncolor=None, lg_replace=[], no_title=False, points=None, vlines=None, orders=None):
+             ncolor=None, lg_replace=[], no_title=False, points=None, xlim=None, vlines=None, orders=None):
     xlabel = {}
     ylabel = {'Tacc': 'Training Accuracy (%)', 'Terror': 'Training Error (%)',
               'train/accuracy': 'Training Accuracy (%)',
@@ -183,7 +183,7 @@ def plot_tag(data, plot_f, run_names, tag_name, lg_tags, ylim=None, color0=0,
               'est_snr': 'Optimization Step SNR',
               'est_nvar': 'Optimization Step Normalized Variance (w/o lr)',
               }
-    yscale_log = ['est_var', 'Vloss', 'Tloss']  # , 'est_var'
+    yscale_log = ['est_var', 'Tloss']  # , 'est_var'
     yscale_log_offset= ['est_var']  # , 'est_var'
     yscale_base = []
     # yscale_sci = ['est_bias', 'est_var']
@@ -203,10 +203,10 @@ def plot_tag(data, plot_f, run_names, tag_name, lg_tags, ylim=None, color0=0,
         data = [data]
         run_names = [run_names]
 
-    color = ['blue', 'orangered', 'limegreen', 'darkkhaki', 'cyan', 'grey']
+    color = ['blue', 'orangered', 'darkred', 'darkkhaki', 'darkblue', 'grey']
     color = color[:ncolor]
     style = ['-', '--', ':', '-.']
-    # plt.rcParams.update({'font.size': 12})
+    plt.rcParams.update({'font.size': 16})
     plt.grid(linewidth=1)
     legends = []
     # extract run index
@@ -214,7 +214,7 @@ def plot_tag(data, plot_f, run_names, tag_name, lg_tags, ylim=None, color0=0,
                for i in range(len(run_names))]
     s_indexes = np.argsort(indexes)
     
-    for i in s_indexes:
+    for i in range(len(data)):
         if tag_name not in data[i]:
             continue
         legends += [get_legend(lg_tags, run_names[i], lg_replace)]
@@ -225,7 +225,7 @@ def plot_tag(data, plot_f, run_names, tag_name, lg_tags, ylim=None, color0=0,
         plot_fs[tag_name](
             data[i][tag_name][0], data[i][tag_name][1], points[i][tag_name],
             vlines=vlines,
-            linestyle=style[(color_index) // len(color)],
+            linestyle=style[(color_index) // len(color)], label=legends[-1],
             color=color[(color_index) % len(color)], linewidth=2)
     if not no_title:
         plt.title(titles[tag_name])
@@ -245,11 +245,30 @@ def plot_tag(data, plot_f, run_names, tag_name, lg_tags, ylim=None, color0=0,
     ax.ticklabel_format(axis='x', style='sci', scilimits=(0, 0))
     if ylim is not None:
         plt.ylim(ylim)
-    plt.xlim([0, 80*1e3])
-    plt.legend(legends, loc="upper left", bbox_to_anchor=(1.01, 1.0), prop={'size': 12})
+    handles, labels = plt.gca().get_legend_handles_labels()
+    norders = []
+    for order in orders:
+        if order in labels:
+            norders.append(order)
+
+    order = []
+    
+    for label in labels:
+        order.append(norders.index(label))
+    
+    nlabels = np.arange(len(labels)).tolist()
+    nhandles = np.arange(len(handles)).tolist()
+    for idx, label, handle in zip(order, labels, handles):
+        nlabels[idx] = label
+        nhandles[idx] = handle
+
+    plt.legend(nhandles, nlabels,
+               loc="upper left", bbox_to_anchor=(1.01, 1.0), prop={'size': 12})
     if vlines:
         for vline in vlines:
             plt.axvline(vline, linestyle='--', color='black')
+    if xlim:
+        plt.xlim(xlim)
     plt.xlabel(xlabel[tag_name])
     plt.ylabel(ylabel[tag_name])
 
@@ -263,6 +282,7 @@ def ticks_10(y, pos):
 def plot_runs_and_tags(get_data_f, plot_f, logdir, patterns, tag_names,
                        fig_name, lg_tags, ylim, batch_size=None, sep_h=True,
                        ncolor=None, save_single=False, lg_replace=[],
+                       xlim=None,
                        no_title=False, vlines=None, color_order=None):
     run_names = get_run_names_events(logdir, patterns)
     data, points = get_data_f(logdir, run_names, tag_names, batch_size)
@@ -291,7 +311,7 @@ def plot_runs_and_tags(get_data_f, plot_f, logdir, patterns, tag_names,
         if not save_single:
             plt.subplot(height, width, fi)
         plot_tag(data, plot_f, list(run_names), tag_names[i], lg_tags, yl,
-                 ncolor=ncolor, lg_replace=lg_replace, no_title=no_title, points=points, vlines=vlines, orders=color_order)
+                 ncolor=ncolor, lg_replace=lg_replace, no_title=no_title, points=points, vlines=vlines, xlim=xlim, orders=color_order)
         if save_single:
             plt.savefig('%s/%s.pdf' % (fig_dir, tag_names[i]),
                         dpi=100, bbox_inches='tight')
@@ -299,3 +319,53 @@ def plot_runs_and_tags(get_data_f, plot_f, logdir, patterns, tag_names,
         fi += 1
     plt.savefig(fig_name, dpi=100, bbox_inches='tight')
     return data, run_names
+
+def find_largest_common_iteration(iters):
+    intersect = set(iters[0])
+    for i in range(1, len(iters)):
+        intersect = intersect & set(iters[i])
+    return list(intersect)
+
+
+def get_accuracies(patterns, lg_replace, lg_tags, log_dir, latex=False):
+    run_names = get_run_names_events(log_dir, patterns)
+    tags = ['Vacc', 'Tacc']
+    data = get_data_pth_events(log_dir, run_names, ['Vacc', 'Tacc'])[0]
+    run_names = list(run_names)
+    results = {} 
+    for i in range(len(tags)):
+        results[tags[i]] = []
+        legends = []
+        iters = []
+        res_i = []
+        for j in range(len(data)):
+            if tags[i] not in data[j]:
+                continue
+            legends += [get_legend(lg_tags, run_names[j], lg_replace)]
+            iters.append(data[j][tags[i]][0])
+        if len(iters) == 0:
+            continue
+        max_iters = find_largest_common_iteration(iters)
+        max_iters = sorted(max_iters)
+        max_iters.reverse()
+        max_iters = max_iters[0:1]
+        for j in range(len(data)):
+            if tags[i] not in data[j]:
+                continue
+            local_result = []
+            for iter in max_iters:
+                index = data[j][tags[i]][0].tolist().index(iter)
+                res = data[j][tags[i]][1][index]
+                local_result.append(res)
+            res_i.append((np.sqrt(np.var(local_result)), np.mean(local_result)))
+        results[tags[i]].append([*zip(res_i, legends)])
+    if latex == True:
+        for key, val in results.items():
+            print('=======', key, '========')
+            if len(val) == 0:
+                continue
+            val_s = sorted(val[0], key=lambda x: x[1])
+            for res in val_s:
+                acc = res[0]
+                print(('%s %.2f\\%% $\pm$ %.2f') % (res[1], acc[1], acc[0]))
+    return results
