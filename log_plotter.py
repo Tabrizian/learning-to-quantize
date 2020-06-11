31a6d10a517c4ed6e18e7bade854d361f39c708e
Iman Tabrizian <tabrizian@outlook.com>
2020-06-11 14:38:43 -0400

    update log_plotter

--- log_plotter.py
+++ log_plotter.py
@@ -1,4 +1,5 @@
 from scipy import interpolate
+import collections
 import numpy as np
 import os
 import re
@@ -129,6 +130,22 @@ class OOMFormatter(mtick.ScalarFormatter):
         else:
             self.acc_bits = 3
 
+    def __call__(self, x, pos=None):
+        """
+        Return the format for tick value *x* at position *pos*.
+        """
+        if len(self.locs) == 0:
+            return ''
+        else:
+            xp = (x - self.offset) / (10. ** self.orderOfMagnitude)
+            if abs(xp) < 1e-8:
+                xp = 0
+            if self._useLocale:
+                s = locale.format_string(self.format, (xp,))
+            else:
+                s = self.format % xp
+            return self.fix_minus(s)
+
 
     def _set_format(self):
         bits = self.acc_bits
@@ -165,7 +182,7 @@ class OOMFormatter(mtick.ScalarFormatter):
             self.format = r'$\mathdefault{%s}$' % self.format
 
 def plot_tag(data, plot_f, run_names, tag_name, lg_tags, ylim=None, color0=0,
-             ncolor=None, lg_replace=[], no_title=False, points=None, xlim=None, vlines=None, orders=None, acc_bits=None):
+             ncolor=None, lg_replace=[], no_title=False, points=None, xlim=None, vlines=None, orders=None, acc_bits=None, markeroff=True):
     xlabel = {}
     ylabel = {'Tacc': 'Training Accuracy (%)', 'Terror': 'Training Error (%)',
               'train/accuracy': 'Training Accuracy (%)',
@@ -192,8 +209,10 @@ def plot_tag(data, plot_f, run_names, tag_name, lg_tags, ylim=None, color0=0,
               'est_snr': 'Optimization Step SNR',
               'est_nvar': 'Optimization Step Normalized Variance (w/o lr)',
               }
-    yscale_log = ['est_var', 'Tloss']  # , 'est_var'
-    yscale_log_offset= ['est_var']  # , 'est_var'
+    yscale_log = ['Tloss', 'est_var']  # , 'est_var'
+    yscale_log_offset= ['Vloss']  # , 'est_var'
+    yscale_scalar= ['Vloss']  # , 'est_var'
+
     yscale_base = []
     # yscale_sci = ['est_bias', 'est_var']
     plot_fs = {'Tacc': plot_f, 'Vacc': plot_f,
@@ -212,9 +231,27 @@ def plot_tag(data, plot_f, run_names, tag_name, lg_tags, ylim=None, color0=0,
         data = [data]
         run_names = [run_names]
 
-    color = ['blue', 'orangered', 'darkred', 'darkkhaki', 'darkblue', 'grey']
+    # color = ['blue', 'orangered', 'darkred', 'darkkhaki', 'darkblue', 'grey']
+    color = [[0.00784314, 0.24313725, 1.],
+             [1., 0.48627451, 0.],
+             [0.10196078, 0.78823529, 0.21960784],
+             [0.90980392, 0., 0.04313725],
+             [0.54509804, 0.16862745, 0.88627451]]
     color = color[:ncolor]
-    style = ['-', '--', ':', '-.']
+    #style = ['-', '--', ':', '-.']
+    style = ['-']
+    color = [[0.00784314, 0.24313725, 1.],
+             [1., 0.48627451, 0.],
+             [0.10196078, 0.78823529, 0.21960784],
+             [0.90980392, 0., 0.04313725],
+             [0.54509804, 0.16862745, 0.88627451]]
+    #style = ['-', '--', ':', '-.']
+    styles = ['-']
+
+#     markers = 
+    colors = color
+#     styles = ['-', '--', ':', '-.']
+    markers = ['o', 'X', 'p', '*', 'd', 'v']
     plt.rcParams.update({'font.size': 16})
     plt.grid(linewidth=1)
     legends = []
@@ -231,11 +268,18 @@ def plot_tag(data, plot_f, run_names, tag_name, lg_tags, ylim=None, color0=0,
             color_index = orders.index(legends[-1])
         else:
             color_index = color0 + i
-        plot_fs[tag_name](
-            data[i][tag_name][0], data[i][tag_name][1], points[i][tag_name],
-            vlines=vlines,
-            linestyle=style[(color_index) // len(color)], label=legends[-1],
-            color=color[(color_index) % len(color)], linewidth=2)
+        if not markeroff:
+            plot_fs[tag_name](
+                data[i][tag_name][0], data[i][tag_name][1], points[i][tag_name],
+                vlines=vlines,
+                linestyle=style[0], label=legends[-1],
+                color=color[(color_index) % len(color)], linewidth=2,  marker=markers[(color_index) % len(markers)], markersize=10, markevery=10+ 2*(color_index%5))
+        else:
+            plot_fs[tag_name](
+                data[i][tag_name][0], data[i][tag_name][1], points[i][tag_name],
+                vlines=vlines,
+                linestyle=style[0], label=legends[-1],
+                color=color[(color_index) % len(color)], linewidth=2)
     if not no_title:
         plt.title(titles[tag_name])
     if tag_name in yscale_log:
@@ -246,32 +290,48 @@ def plot_tag(data, plot_f, run_names, tag_name, lg_tags, ylim=None, color0=0,
         else:
             ax.set_yscale('log')
             if tag_name in yscale_log_offset:
-                ax.yaxis.set_major_formatter(OOMFormatter(useOffset=True, acc_bits=acc_bits))
+                ax.yaxis.set_major_formatter(mtick.ScalarFormatter(useOffset=True))
+                ax.yaxis.set_major_formatter(mtick.ScalarFormatter(useOffset=True))
                 ax.ticklabel_format(axis='y', style='sci', scilimits=(0, 0))
+                print('Hone')
     else:
         ax = plt.gca()
         ax.ticklabel_format(axis='y', style='sci', scilimits=(-3, 3))
+    
+    if tag_name in yscale_scalar:
+        ax.set_yscale('log')
+        ax.yaxis.set_minor_locator(mtick.LogLocator(base=10.0, subs=[2,4,6]))
+        ax.yaxis.set_minor_formatter(mtick.ScalarFormatter())
+        ax.yaxis.set_major_formatter(OOMFormatter(acc_bits=1))
+        #ax.ticklabel_format(axis='y', style='sci', scilimits=(0, 0))
+
+    
     ax.ticklabel_format(axis='x', style='sci', scilimits=(0, 0))
     if ylim is not None:
         plt.ylim(ylim)
     handles, labels = plt.gca().get_legend_handles_labels()
-    norders = []
-    for order in orders:
-        if order in labels:
-            norders.append(order)
 
-    order = []
-    
-    for label in labels:
-        order.append(norders.index(label))
-    
-    nlabels = np.arange(len(labels)).tolist()
-    nhandles = np.arange(len(handles)).tolist()
-    for idx, label, handle in zip(order, labels, handles):
-        nlabels[idx] = label
-        nhandles[idx] = handle
+    if orders:
+        norders = []
+        for order in orders:
+            if order in labels:
+                norders.append(order)
 
-    plt.legend(nhandles, nlabels,
+        order = []
+    
+        for label in labels:
+            order.append(norders.index(label))
+    
+        nlabels = np.arange(len(labels)).tolist()
+        nhandles = np.arange(len(handles)).tolist()
+        for idx, label, handle in zip(order, labels, handles):
+            nlabels[idx] = label
+            nhandles[idx] = handle
+        handles = nhandles
+        labels = nlabels
+        
+
+    plt.legend(handles, labels,
                loc="upper left", bbox_to_anchor=(1.01, 1.0), prop={'size': 12})
     if vlines:
         for vline in vlines:
@@ -291,7 +351,7 @@ def ticks_10(y, pos):
 def plot_runs_and_tags(get_data_f, plot_f, logdir, patterns, tag_names,
                        fig_name, lg_tags, ylim, batch_size=None, sep_h=True,
                        ncolor=None, save_single=False, lg_replace=[],
-                       xlim=None, acc_bits=None,
+                       xlim=None, acc_bits=None, markeroff=True,
                        no_title=False, vlines=None, color_order=None):
     run_names = get_run_names_events(logdir, patterns)
     data, points = get_data_f(logdir, run_names, tag_names, batch_size)
@@ -321,7 +381,7 @@ def plot_runs_and_tags(get_data_f, plot_f, logdir, patterns, tag_names,
             plt.subplot(height, width, fi)
         plot_tag(data, plot_f, list(run_names), tag_names[i], lg_tags, yl,
                  ncolor=ncolor, lg_replace=lg_replace, no_title=no_title, points=points, vlines=vlines, xlim=xlim, orders=color_order,
-                 acc_bits=acc_bits)
+                 acc_bits=acc_bits, markeroff=markeroff)
         if save_single:
             plt.savefig('%s/%s-lo.pdf' % (fig_dir, tag_names[i]),
                         dpi=100, bbox_inches='tight')
@@ -329,8 +389,26 @@ def plot_runs_and_tags(get_data_f, plot_f, logdir, patterns, tag_names,
             ax = plt.gca()
 
             handles, labels = ax.get_legend_handles_labels()
-            plt.legend(handles, labels,
-               loc="upper left", prop={'size': 12})
+            if color_order:
+                norders = []
+                for order in color_order:
+                    if order in labels:
+                        norders.append(order)
+
+                order = []
+
+                for label in labels:
+                    order.append(norders.index(label))
+
+                nlabels = np.arange(len(labels)).tolist()
+                nhandles = np.arange(len(handles)).tolist()
+                for idx, label, handle in zip(order, labels, handles):
+                    nlabels[idx] = label
+                    nhandles[idx] = handle
+                handles = nhandles
+                labels = nlabels
+
+            plt.legend(handles, labels, prop={'size': 12})
 
             plt.savefig('%s/%s-li.pdf' % (fig_dir, tag_names[i]),
                         dpi=100, bbox_inches='tight')
@@ -351,10 +429,10 @@ def find_largest_common_iteration(iters):
     return list(intersect)
 
 
-def get_accuracies(patterns, lg_replace, lg_tags, log_dir, latex=False):
+def get_accuracies(patterns, lg_replace, lg_tags, log_dir, latex=False, notitle=False, onlyTitle=False):
     run_names = get_run_names_events(log_dir, patterns)
     tags = ['Vacc', 'Tacc']
-    data = get_data_pth_events(log_dir, run_names, ['Vacc', 'Tacc'])[0]
+    data = get_data_pth_events(log_dir, run_names, tags)[0]
     run_names = list(run_names)
     results = {} 
     for i in range(len(tags)):
@@ -372,7 +450,7 @@ def get_accuracies(patterns, lg_replace, lg_tags, log_dir, latex=False):
         max_iters = find_largest_common_iteration(iters)
         max_iters = sorted(max_iters)
         max_iters.reverse()
-        max_iters = max_iters[0:1]
+        max_iters = max_iters[0:5]
         for j in range(len(data)):
             if tags[i] not in data[j]:
                 continue
@@ -389,7 +467,105 @@ def get_accuracies(patterns, lg_replace, lg_tags, log_dir, latex=False):
             if len(val) == 0:
                 continue
             val_s = sorted(val[0], key=lambda x: x[1])
+            
             for res in val_s:
                 acc = res[0]
-                print(('%s %.2f\\%% $\pm$ %.2f') % (res[1], acc[1], acc[0]))
+                if onlyTitle:
+                    print(res[1])
+                    continue
+
+                if not notitle:
+                    print(('%s & %.2f\\%% $\pm$ %.2f') % (res[1], acc[1], acc[0]))
+                if notitle:
+                    print(('%.2f\\%% $\pm$ %.2f') % (acc[1], acc[0]))
     return results
+
+def plot_levels(levels, filename):
+    key = list(levels)[0]
+    level_indexes = np.arange(0, len(levels[key]))
+    styles = ['-']
+    orders = [ 'ALQ', 'AMQ', 'ALQ-N', 'AMQ-N','Qinf', 'TRN', 'NUQ,p=0.5', 'SignSGD', 'SignSGDInf']
+
+#     markers = 
+#     styles = ['-', '--', ':', '-.']
+    markers = ['o', 'X', 'p', '*', 'd', 'v']
+    orders = [ 'ALQ', 'AMQ', 'ALQ-N', 'AMQ-N','Qinf', 'TRN', 'NUQ,p=0.5', 'SignSGD', 'SignSGDInf']
+    colors = [[0.00784314, 0.24313725, 1.],
+             [1., 0.48627451, 0.],
+             [0.10196078, 0.78823529, 0.21960784],
+             [0.90980392, 0., 0.04313725],
+             [0.54509804, 0.16862745, 0.88627451]]
+
+    index = 0
+    levels = collections.OrderedDict(sorted(levels.items()))
+    print(levels)
+    for level, label in zip(levels.values(), list(levels)):
+        index = orders.index(label)
+        if len(level) == 3:
+            plt.plot(level, [0, 3, 7], markers[index % len(markers)], label=label, color=colors[index % len(colors)], markersize=15-index)
+            continue
+        plt.plot(level, level_indexes, markers[index % len(markers)], label=label, color=colors[index % len(colors)], markersize=15-index)
+    handles, labels = plt.gca().get_legend_handles_labels()
+
+#     plt.title(ytitle + ' vs ' + xtitle)
+    norders = []
+    for order in orders:
+        if order in labels:
+            norders.append(order)
+
+    order = []
+    
+    for label in labels:
+        order.append(norders.index(label))
+    
+    nlabels = np.arange(len(labels)).tolist()
+    nhandles = np.arange(len(handles)).tolist()
+    for idx, label, handle in zip(order, labels, handles):
+        nlabels[idx] = label
+        nhandles[idx] = handle
+    print(nlabels)
+    dirn = 'figs_levels/'
+    plt.savefig(dirn + filename +'.pdf', dpi=100, bbox_inches='tight')
+    plt.legend(nhandles, nlabels, bbox_to_anchor=(1.01, 1.0))
+    plt.savefig(dirn+'lo-'+filename + '.pdf', dpi=100, bbox_inches='tight')
+    plt.legend(nhandles, nlabels)
+    plt.savefig(dirn+'li-'+filename + '.pdf', dpi=100, bbox_inches='tight')
+
+
+def get_levels(patterns, lg_replace, lg_tags, log_dir, num_of_levels=8):
+    run_names = get_run_names_events(log_dir, patterns)
+    tags = []
+    for i in range(num_of_levels):
+        tags.append('levels/'+str(i))
+
+    data = get_data_pth_events(log_dir, run_names, tags)[0]
+    run_names = list(run_names)
+    results = {} 
+    for i in range(len(tags)):
+        results[tags[i]] = []
+        legends = []
+        iters = []
+        res_i = []
+        for j in range(len(data)):
+            if tags[i] not in data[j]:
+                continue
+            legends += [get_legend(lg_tags, run_names[j], lg_replace)]
+            iters.append(data[j][tags[i]][0])
+        if len(iters) == 0:
+            continue
+        max_iters = find_largest_common_iteration(iters)
+        max_iters = sorted(max_iters)
+        max_iters.reverse()
+        max_iters = max_iters[0:5]
+        for j in range(len(data)):
+            if tags[i] not in data[j]:
+                continue
+            local_result = []
+            for iter in max_iters:
+                index = data[j][tags[i]][0].tolist().index(iter)
+                res = data[j][tags[i]][1][index]
+                local_result.append(res)
+            res_i.append(np.mean(local_result))
+        results[tags[i]].append([*zip(res_i, legends)])
+    return results
+
